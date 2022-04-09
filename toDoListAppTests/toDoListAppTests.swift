427222d5035cc872642bc595a2f3b30c94d3c2c7
nobath-nogas <ch1ky1job@gmail.com>
2022-04-09 23:21:19 +0900

    環境構築：Test

--- toDoListAppTests/toDoListAppTests.swift
+++ toDoListAppTests/toDoListAppTests.swift
@@ -11,6 +11,8 @@ import XCTest
 class toDoListAppTests: XCTestCase {
 
     override func setUpWithError() throws {
+        print("AAAA")
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
